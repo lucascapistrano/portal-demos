9bfca1f2766dc6f6644f940f6fed6c0b93a75acc
Ralph Schaer <ralphschaer@gmail.com>
2013-01-21 04:21:37 +0100

    Add smoothiecharts example

---
+++ src/main/java/ch/rasc/portaldemos/smoothie/RandomDataHandler.java
@@ -0,0 +1,59 @@
+package ch.rasc.portaldemos.smoothie;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.github.flowersinthesand.portal.App;
+import com.github.flowersinthesand.portal.Handler;
+import com.github.flowersinthesand.portal.Name;
+import com.github.flowersinthesand.portal.On;
+import com.github.flowersinthesand.portal.Room;
+import com.github.flowersinthesand.portal.Socket;
+
+@Handler("/smoothie")
+public class RandomDataHandler {
+
+	private static ScheduledExecutorService threadPool;
+
+	private static Random random = new Random();
+
+	@Name("smoothie")
+	Room room;
+
+	@On.close
+	public void close(Socket socket) {
+		System.out.println("closing: " + socket);
+		if (room.size() == 0) {
+			threadPool.shutdownNow();
+			threadPool = null;
+		}
+	}
+
+	@On.open
+	public void open(Socket socket) {
+		room.add(socket);
+		if (threadPool == null) {
+			threadPool = Executors.newScheduledThreadPool(1);
+			threadPool.scheduleWithFixedDelay(new RandomDataGenerator(), 1, 1, TimeUnit.SECONDS);
+		}
+	}
+
+	private class RandomDataGenerator implements Runnable {
+		@Override
+		public void run() {
+			CpuData cpuData = new CpuData();
+			cpuData.setHost1(new double[] { random.nextDouble(), random.nextDouble(), random.nextDouble(),
+					random.nextDouble() });
+			cpuData.setHost2(new double[] { random.nextDouble(), random.nextDouble(), random.nextDouble(),
+					random.nextDouble() });
+			cpuData.setHost3(new double[] { random.nextDouble(), random.nextDouble(), random.nextDouble(),
+					random.nextDouble() });
+			cpuData.setHost4(new double[] { random.nextDouble(), random.nextDouble(), random.nextDouble(),
+					random.nextDouble() });
+			App.find("/smoothie").room("smoothie").send("cpu", cpuData);
+
+		}
+	}
+}
